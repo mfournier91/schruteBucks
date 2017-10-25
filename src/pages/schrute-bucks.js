2645cb92e17907e42f6801f67a5edba104487704
Marc Fournier <marc.fournier91@gmail.com>
2017-10-24 22:57:29 -0400

    Adds how it works

--- src/pages/schrute-bucks.js
+++ src/pages/schrute-bucks.js
@@ -8,6 +8,8 @@ export default () =>
     <div className={indexStyles.header}>
       <h1>Buy Now!</h1>
       <h3>Legitimate Currency!</h3>
-
+        <br />
+        <h1>How it works</h1>
+        <iframe width="854" height="480" src="https://www.youtube.com/embed/gT_nb14b_QM" frameborder="0" gesture="media" allowfullscreen></iframe>
     </div>
   </div>
